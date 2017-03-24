349a17f72d4fbf447e60c4620274289c83440132
Chris Wilkening <clw13clw13@gmail.com>
2017-03-24 11:19:20 -0400

    render Product Details section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.selectVendor = this.selectVendor.bind(this);
+    this.productDetails = this.productDetails.bind(this);
+
     this.state = {
-      data: "",
+      data: [],
     };
   };
 
@@ -16,8 +19,68 @@ class App extends Component {
     this.setState({ data }); //set data to state so that it can be used throughout app
   }
 
-productDetails(){
+  selectVendor(){
+    let vendorOptions = [];
+    let vendors = this.state.data.vendors;
+    //console.log(vendors)
+    for (let key in vendors){
+      let vendor = vendors[key]
+        vendorOptions.push(
+          <option value={vendor}>{vendor}</option>
+        )
+    }
+    return(vendorOptions);
+  }
 
+  productDescription(){
+    let descriptionOptions = [];
+    let descriptions = this.state.data.descriptions;
+    for (let key in descriptions){
+      let description = descriptions[key]
+        descriptionOptions.push(
+          <option value={description}>{description}</option>
+        )
+    }
+    return(descriptionOptions);
+  }
+
+  productUOM(){
+    let uomOptions = [];
+    let uoms = this.state.data.UOM;
+    for (let key in uoms){
+      let uom = uoms[key]
+        uomOptions.push(
+          <option value={uom}>{uom}</option>
+        )
+    }
+    return(uomOptions);
+  }
+
+
+
+  productDetails(){
+    return(
+    <FormGroup controlId="formControlsSelect">
+      <ControlLabel>Select Vendor*</ControlLabel>
+      <FormControl componentClass="select" placeholder="Vendors">
+        {this.selectVendor()}
+      </FormControl>
+      <ControlLabel>Product Description*</ControlLabel>
+      <FormControl componentClass="select" placeholder="Product Description">
+        {this.productDescription()}
+      </FormControl>
+      <ControlLabel>Product UOM*</ControlLabel>
+      <FormControl componentClass="select" placeholder="UOM">
+        {this.productUOM()}
+      </FormControl>
+      <ControlLabel>Add to Catalog?*</ControlLabel>
+      <FormControl componentClass="select" placeholder="Add to Catalog?">
+        <option value="...">...</option>
+        <option value="yes">yes</option>
+        <option value="no">no</option>
+      </FormControl>
+    </FormGroup>
+  )
 }
 
 
@@ -28,7 +91,7 @@ productDetails(){
         <div className="App-header">
           <h2>Product Form</h2>
         </div>
-
+      {this.productDetails()}
       </div>
     );
   }
